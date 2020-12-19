80aef993460d45898bf40db1d81377a49d6dea1a
Dhanush reddy <Dhanushreddy09@users.noreply.github.com>
2020-12-19 12:40:26 +0530

    design check

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,37 +1,28 @@
 <template>
-<div class="bg-gray-800">
- 
-<div class="text-center py-8">
-  <span id="projects" class=" font-black bg-clip-text text-transparent bg-gradient-to-r from-green-500  to-blue-500 text-5xl">PROJECTS</span>
-  <br/>
-  <div v-for="project in projects" :key="project.id" class="md:m-14 m-10  h-80 w-80 md:float-left rounded-xl shadow-lg py-6 bg-gradient-to-r from-blue-400  to-purple-500">
-       <div class="h-16 w-full py-3 m-auto">
-         <span class="text-2xl font-black px-6 bg-clip-text text-transparent bg-gradient-to-r from-indigo-700  to-pink-700">{{project.Name}}</span>
-       </div>
-       <br/>
-       <div class="h-28 w-full py-3 text-center  m-auto break-words">
-         <span class="text-md font-medium">
-          {{project.Description}}
-         </span>
-       </div>
-       <br/>
-       <br/>
-       <div class="px-3 space-x-12">
-         <a :href="project.livedemolink" target="blank" class="bg-pink-600 cursor-pointer hover:bg-pink-700 focus:outline-none text-white font-bold py-2 px-4 rounded-xl shadow-lg">
- Live Demo  <i class="fa fa-play text-sm text-yellow-400" aria-hidden="true"></i>
-</a>
-<a :href="project.gitlink" target="blank" class=" bg-pink-600 hover:bg-pink-700 focus:outline-none text-white font-bold py-2 px-4 rounded-xl shadow-lg">
- Github <img class="h-5 w-5 inline" :src="markgithub" />
-</a>
-       </div>
+<div class="bg-gray-800 h-full">
+    <div class="text-white h-28 grid justify-items-center text-opacity-70 py-10">
+      <ul class="md:space-x-10 space-x-8">
+        <li class="float-left cursor-pointer px-3 hover:bg-green-50 hover:text-black rounded-md hover:shadow-lg">ABOUT</li>
+    <li class="float-left cursor-pointer px-3 hover:bg-green-50 hover:text-black rounded-md hover:shadow-lg"><a href="#projects">PROJECTS</a></li>
+    <li class="float-left cursor-pointer px-3 hover:bg-green-50 hover:text-black rounded-md hover:shadow-lg">BLOGS</li>
+      </ul>
+  </div>
+ <div class="h-96 w-2/5 px-3  text-white text-opacity-60  md:px-16 ">
+     <div class="h-96 w-96 bg-green-50 rounded-xl shadow-lg">
+  <div class="h-96 w-96 m-auto max-w-full bg-pink-600 rounded-xl shadow-lg transform -rotate-6">
+     <p class="text-4xl font-black px-24 py-4">Hello, I'm</p>
+     <p class="text-5xl font-black px-4 py-4">Dhanush</p>
+     <p class="text-5xl font-black px-20 py-0">Pothulapati</p>
+     <p class="text-xl px-14 py-10">A promising web developer</p>
+     <div class="px-32">
+     <button class="bg-blue-500 hover:bg-blue-600 focus:outline-none text-white font-bold py-2 px-4 rounded-xl shadow-lg">
+  Resume
+</button>
+</div>
+  </div>
   </div>
 </div>
-<div class="text-center w-screen h-7">
-  <a href="https://github.com/Dhanushreddy09" target="blank" class="bg-pink-600 hover:bg-pink-700 focus:outline-none text-white font-bold py-2 px-4 rounded-xl shadow-lg">
- More Projects ? <img class="h-5 w-5 inline" :src="markgithub" />
-</a>
-</div>
-<p class="text-gray-500 text-center w-screen ">&copy 2020 Dhanush Pothulapati</p>
+ 
 </div>
 
 </template>
