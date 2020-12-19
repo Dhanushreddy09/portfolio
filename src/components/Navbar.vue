c3621541f00d31a62a9e1219edaa49cd2e0d009c
Dhanush reddy <Dhanushreddy09@users.noreply.github.com>
2020-12-19 22:02:00 +0530

    page check

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,37 +2,64 @@
 <div class="bg-gray-800 h-full">
     <div class="text-white h-28 grid justify-items-center text-opacity-70 py-10">
       <ul class="md:space-x-10 space-x-8">
-        <li class="float-left cursor-pointer px-3 hover:bg-green-50 hover:text-black rounded-md hover:shadow-lg">ABOUT</li>
+        <li class="float-left cursor-pointer px-3 hover:bg-green-50 hover:text-black rounded-md hover:shadow-lg"><a href="#about">ABOUT</a></li>
     <li class="float-left cursor-pointer px-3 hover:bg-green-50 hover:text-black rounded-md hover:shadow-lg"><a href="#projects">PROJECTS</a></li>
     <li class="float-left cursor-pointer px-3 hover:bg-green-50 hover:text-black rounded-md hover:shadow-lg">BLOGS</li>
       </ul>
   </div>
- <div class="h-full text-center text-white content-around">
+ <div class="h-full text-center text-white content-around py-20">
    <div>
-     <p class="font-black text-4xl md:text-5xl text-opacity-60">Hello, I'm</p>
+     <p class="font-black text-4xl md:text-5xl text-opacity-60 bg-clip-text text-transparent bg-gradient-to-r to-pink-500  from-purple-500">Hello, I'm</p>
      <br/>
-     <p class="font-black text-4xl md:text-5xl text-opacity-60">Dhanush Potulapati</p>
+     <p class="font-black text-4xl md:text-5xl text-opacity-60 bg-clip-text text-transparent bg-gradient-to-r to-indigo-600  from-blue-400">Dhanush Potulapati</p>
+     <br/>
+     <br/>
+     <p class="text-xl text-opacity-80">A promising web developer</p>
+     <br/>
      <br/>
      <button class="bg-blue-500 hover:bg-blue-600 focus:outline-none text-white font-bold py-2 px-4 rounded-xl shadow-lg">
   Resume
 </button>
  </div>
- 
+ <br/>
+ <br/>
+ <div class="grid justify-items-center">
+ <ul class="md:space-x-10 space-x-8">
+   <li class="float-left px-3  cursor-pointer hover:shadow-lg">
+      <a href="https://www.linkedin.com/in/dhanush-pothulapati-5236b2165/" target="blank">
+        <img class="h-7 w-17" :src="linkedin" />
+      </a>
+    </li>
+    <li class="float-left px-3  cursor-pointer hover:shadow-lg">
+      <a href="https://twitter.com/Dhanushreddy09" target="blank">
+        <img class="h-7 w-17" :src="twitter" />
+      </a>
+    </li>
+    <li class="float-left px-3  cursor-pointer hover:shadow-lg">
+      <a href="https://github.com/Dhanushreddy09" target="blank">
+        <img class="h-7 w-17" :src="github" />
+      </a>
+    </li>
+ </ul>
+ </div>
+</div>
+<div class="text-center py-8">
+  <span id="about" class=" font-black bg-clip-text text-transparent bg-gradient-to-r from-green-500  to-blue-500 text-5xl">ABOUT</span>
 </div>
 </div>
 
 </template>
 <script>
-/*import linkedinimage from '../../assets/NicePng_linkedin-logo-png_233614.png'
+import linkedinimage from '../../assets/NicePng_linkedin-logo-png_233614.png'
 import githubimage from '../../assets/GitHub-Mark-Light-32px.png'
-import twitterimage from '../../assets/Twitter_Social_Icon_Circle_White.png'*/
+import twitterimage from '../../assets/Twitter_Social_Icon_Circle_White.png'
 import githubmark from '../../assets/GitHub-Mark-32px.png'
 export default {
   data() {
     return {
-      /*linkedin:linkedinimage,
+      linkedin:linkedinimage,
       github:githubimage,
-      twitter:twitterimage,*/
+      twitter:twitterimage,
       markgithub:githubmark,
      projects:[
        {
