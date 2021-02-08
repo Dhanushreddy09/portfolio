4acad6d54962eac0176fa261623fb0f997b4333f
Dhanush reddy <Dhanushreddy09@users.noreply.github.com>
2021-02-08 12:30:38 +0530

    resume update

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -18,7 +18,7 @@
      <p class="text-xl text-opacity-80">A promising web developer</p>
      <br/>
      <br/>
-     <a href="https://drive.google.com/file/d/1nPEIjr9y9YLT-MvIvbcBwK04G6Gtz1MY/view?usp=sharing" target="blank" class="bg-blue-500 hover:bg-blue-600 focus:outline-none text-white font-bold py-2 px-4 rounded-xl shadow-lg">
+     <a href="https://drive.google.com/file/d/1TgkFtDkGgYCw9ugPuFIMYkL7GtPO3PFp/view?usp=sharing" target="blank" class="bg-blue-500 hover:bg-blue-600 focus:outline-none text-white font-bold py-2 px-4 rounded-xl shadow-lg">
   Resume
 </a>
  </div>
